ecf33769414701a1862ac81c75b0469778258bde
Jakub Marchlewski <kuba.marchlewski@gmail.com>
2024-02-06 08:33:13 +0100

    Use Address components in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from "react";
-import logo from "./logo.svg";
 import styles from "./App.module.scss";
+import AddressForm from "./components/AddressForm/AddressForm";
+import { AddressList } from "./components/AddressList/AddressList";
+import { AddressListProvider } from "./components/AddressListProvider/AddressListProvider";
 
 function App() {
   return (
     <div className={styles.App}>
-      <header className={styles["App-header"]}>
-        <img src={logo} className={styles["App-logo"]} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className={styles["App-link"]}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AddressListProvider>
+        <AddressForm />
+        <AddressList />
+      </AddressListProvider>
     </div>
   );
 }
