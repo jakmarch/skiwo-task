39dce47ebcd90f6de2037f509e6bd994d1619c78
Jakub Marchlewski <kuba.marchlewski@gmail.com>
2024-02-06 08:32:02 +0100

    Address list item

---
+++ src/components/AddressListItem/AddressListItem.tsx
@@ -0,0 +1,31 @@
+import { AddressData } from "../AddressListProvider/AddressListProvider";
+import { DeleteIcon } from "../icons/DeleteIcon";
+import styles from "./AddressListItem.module.scss";
+interface IAddressListItemProps {
+  address: AddressData;
+  onDelete: (id: string) => void;
+}
+
+export const AddressListItem = ({
+  address,
+  onDelete,
+}: IAddressListItemProps) => {
+  return (
+    <div className={styles.addressListItem}>
+      <div className={styles.addressListItem__leftContentWrapper}>
+        <h2 className={styles.addressListItem__heading}>{address.address}</h2>
+        <p className={styles.addressListItem__paragraph}>
+          Nr of departments: {address.departments}
+        </p>
+        <p className={styles.addressListItem__paragraph}>
+          Directions: {address.description ? address.description : "(N/A)"}
+        </p>
+      </div>
+      <div className={styles.addressListItem__rightContentWrapper}>
+        <button onClick={() => onDelete(address.id)}>
+          <DeleteIcon />
+        </button>
+      </div>
+    </div>
+  );
+};
